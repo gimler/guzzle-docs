effb972bcb953b8f5c52cb45d87ec8947e997d0d
Michael Dowling <mtdowling@gmail.com>
2012-01-18 21:20:52 -0600

    Making stuff easier to read

--- conf.py
+++ conf.py
@@ -94,7 +94,7 @@ exclude_patterns = ['build']
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'tango'
+pygments_style = 'colorful'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
