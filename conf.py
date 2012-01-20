2415373a4107d288575a85c0a9e08e1d6b8f89c3
Michael Dowling <mtdowling@gmail.com>
2012-01-19 19:13:50 -0600

    Making the docs easier to read

--- conf.py
+++ conf.py
@@ -94,7 +94,7 @@ exclude_patterns = ['build']
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'colorful'
+pygments_style = 'monokai'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
